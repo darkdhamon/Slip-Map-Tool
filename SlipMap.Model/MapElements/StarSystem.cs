a2d89b23e467e144c67c6f5d3f48b48d42ea8adc
Bronze Brown <dhamon.nightstar@gmail.com>
2022-10-18 11:47:07 -0400

    Create StarSystem.cs

---
+++ SlipMap.Model/MapElements/StarSystem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlipMap.Model.MapElements
+{
+    public class StarSystem
+    {
+        private List<StarSystem> _connectedSystems = null!;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+
+        public List<StarSystem> ConnectedSystems
+        {
+            get => _connectedSystems??= new List<StarSystem>();
+            set => _connectedSystems = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name.IfNullOrWhiteSpace("Unnamed System")} ({ID})";
+        }
+    }
+}
