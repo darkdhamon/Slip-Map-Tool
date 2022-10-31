cfc72cd565c5591b0ffd9fb06e7f6676d0100ec7
Bronze Brown <dhamon.nightstar@gmail.com>
2022-10-31 13:51:50 -0400

    Domain View Models

---
+++ SlipMap.Domain/ViewModels/StarSystemSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SlipMap.Model.MapElements;
+
+namespace SlipMap.Domain.ViewModels
+{
+    public class StarSystemSummary
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public RadialMapCoordinates Coordinates { get; set; }
+    }
+}
