ec6c76ccad4d5978ebf66f1400786f7802be66e6
Bronze Brown <dhamon.nightstar@gmail.com>
2022-11-28 07:29:10 -0500

    Update Planet.cs
    Default Planet Name to Empty

--- SlipMap.Model/MapElements/Planet.cs
+++ SlipMap.Model/MapElements/Planet.cs
@@ -5,5 +5,5 @@ namespace SlipMap.Model.MapElements;
 public class Planet:IEntity<int>
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 }
